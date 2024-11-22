d68fc8dcea06d7cff286a32c818dd4fb904863cb
Paweł Korsak <100282616+PawelKorsak@users.noreply.github.com>
2024-11-22 19:49:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cmake-stm32-template
+STM32 app with nessesary development frameworks and libraries
---
+++ src/application/main.h
@@ -0,0 +1,9 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+extern "C" {
+   void main(void);
+}
+
+#endif /* __MAIN_H */
