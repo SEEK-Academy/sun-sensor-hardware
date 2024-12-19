44112d9b6c08765731e1ab1a6e671ed045e855b5
PawelKorsak <p.korsak010@gmail.com>
2024-12-19 23:00:38 +0100

    feature: draft clock configuration

--- src/application/src/main.cc
+++ src/application/src/main.cc
@@ -1,45 +1,54 @@
 #include "main.h"
 #include "stm32f401xc.h"
+#include <cstdint>
 
-#define LED_PIN 5
+#define PLL_Q 4U
+#define PLL_M 8U
+#define PLL_N 200U
+#define PLL_P 4U
 
-uint32_t ticks;
+volatile uint32_t ticks;
 
-void systick_handler()
+extern "C" void SysTick_Handler()
 {
     ticks++;
 }
 
 void ClockInit()
 {
-    RCC->CR |= RCC_CR_HSEBYP_Msk | RCC_CR_HSEON_Msk;
-    while (!(RCC->CR & RCC_CR_HSERDY_Msk))
-        ;
-    RCC->APB1ENR |= RCC_APB1ENR_PWREN_Msk;
-    volatile uint32_t dummy;
-    dummy = RCC->APB1ENR;
-    dummy = RCC->APB1ENR;
-    PWR->CR |= (0b11 << PWR_CR_VOS_Pos);
-    FLASH->ACR |= FLASH_ACR_LATENCY_3WS;
-    // Clear PLLM, PLLN and PLLP bits
-    RCC->PLLCFGR &= ~(RCC_PLLCFGR_PLLM_Msk | RCC_PLLCFGR_PLLN_Msk | RCC_PLLCFGR_PLLP_Msk);
+    // BlackPill has 25Mhz HSE clock
 
-    // Set PLLM, PLLN and PLLP, and select HSE as PLL source
-    RCC->PLLCFGR |= ((4 << RCC_PLLCFGR_PLLM_Pos) | (200 << RCC_PLLCFGR_PLLN_Pos) |
-                     (1 << RCC_PLLCFGR_PLLP_Pos) | (1 << RCC_PLLCFGR_PLLSRC_Pos));
+    RCC->CR |= RCC_CR_HSION;
+    // RCC->CR |= RCC_CR_HSEON;  // Turn on HSE
+    while (!(RCC->CR & RCC_CR_HSIRDY))
+    {
+        // wait for stable clock
+    }
 
-    // Set APB1 prescaler to 2
-    RCC->CFGR |= (0b100 << RCC_CFGR_PPRE1_Pos);
+    RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+    PWR->CR |= PWR_CR_VOS;
+    FLASH->ACR |= FLASH_ACR_ICEN | FLASH_ACR_PRFTEN | FLASH_ACR_DCEN | FLASH_ACR_LATENCY_3WS;
 
-    // Enable PLL and wait for ready
-    RCC->CR |= RCC_CR_PLLON_Msk;
-    while (!(RCC->CR & RCC_CR_PLLRDY_Msk))
-        ;
+    RCC->PLLCFGR |= (PLL_Q << RCC_PLLCFGR_PLLQ_Pos) | (PLL_P << RCC_PLLCFGR_PLLP_Pos) |
+                    (PLL_N << RCC_PLLCFGR_PLLN_Pos) | (PLL_M << RCC_PLLCFGR_PLLM_Pos) |
+                    RCC_PLLCFGR_PLLSRC_HSI;
+    RCC->CR |= RCC_CR_PLLON;
 
-    // Select PLL output as system clock
-    RCC->CFGR |= (RCC_CFGR_SW_PLL << RCC_CFGR_SW_Pos);
-    while (!(RCC->CFGR & RCC_CFGR_SWS_PLL))
-        ;
+    while (!(RCC->CR & RCC_CR_PLLRDY))
+    {
+        // wait for stable PLL
+    }
+
+    RCC->CFGR |= RCC_CFGR_HPRE_DIV1;   // AHB Prescaler: No division (100 MHz)
+    RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;  // APB1 Prescaler: Divide by 4 (100 MHz / 4 = 25 MHz)
+    RCC->CFGR |= RCC_CFGR_PPRE2_DIV2;  // APB2 Prescaler: Divide by 2 (100 MHz / 2 = 50 MHz)
+
+    RCC->CFGR |= RCC_CFGR_SW_PLL;
+    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL)
+    {
+        // Wait until PLL is used as the system clock source
+    }
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;  // GPIO ON
 
     SysTick_Config(100000);
     __enable_irq();
@@ -53,22 +62,46 @@ void delay_ms(uint32_t milliseconds)
     if (end < start)  // handle overflow
     {
         while (ticks > start)
-            ;  // wait for ticks to wrap around to zero
+        {
+            ;
+        }  // wait for ticks to wrap around to zero
     }
 
     while (ticks < end)
+    {
         ;
+    }
 }
 
-int main(void)
+// void __attribute__((optimize("O0"))) delay(uint32_t time)
+// {
+//     static uint32_t i;
+//     for (i = 0; i < time; i++)
+//     {
+//     }
+// }
+
+extern "C" int main(void)
 {
+    // RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;  // RCC ON
+
+    // GPIOC->MODER |= GPIO_MODER_MODER13_0;  // mode out
+    // GPIOC->OTYPER  = 0;
+    // GPIOC->OSPEEDR = 0;
+
+    // while (1)
+    // {
+    //     GPIOC->ODR ^= GPIO_ODR_OD13;
+    //     delay(1000000);
+    // }
+
     ClockInit();
 
-    GPIOA->MODER |= (1 << GPIO_MODER_MODER5_Pos);
+    GPIOC->MODER |= (1 << GPIO_MODER_MODER13_Pos);
 
     while (1)
     {
-        GPIOA->ODR ^= (1 << LED_PIN);
+        GPIOC->ODR ^= GPIO_ODR_OD13;
         delay_ms(500);
     }
 }
